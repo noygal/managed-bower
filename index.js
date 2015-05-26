c1dc1e8ad8f12124f979d973f2d23b2a61c7428f
Gal Noy <gal@galnoy.com>
2015-05-26 21:03:25 +0300

    Added dependencies

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
 var through = require('through2');
+var bower = require('bower');
+var htmlparser = require("htmlparser2");
+
+function parseHtml(params) {
+	var parser = new htmlparser.Parser({
+		onopentag: function (name, attribs) {
+			if (name === "script" && attribs.type === "text/javascript") {
+				console.log("JS! Hooray!");
+			}
+		},
+		ontext: function (text) {
+			console.log("-->", text);
+		},
+		onclosetag: function (tagname) {
+			if (tagname === "script") {
+				console.log("That's it?!");
+			}
+		}
+	});
+	parser.write("Xyz <script type='text/javascript'>var foo = '<<bar>>';</ script>");
+	parser.end();
+}
+
+function bowerInstall(params) {
+	bower.commands
+		.install(['jquery'], { save: true }, { /* custom config */ })
+		.on('end', function (installed) {
+		console.log(installed);
+	});
+
+}
 function managedBower() {
-	return through.obj(function(fileStream, enc, callback){
+	return through.obj(function (fileStream, enc, callback) {
 		if (fileStream.isBuffer()) {
 			var file = String(fileStream.contents);
 			console.log(file);
@@ -8,6 +39,7 @@ function managedBower() {
 			fileStream.contents = new Buffer(file);
 			callback(null, fileStream);
 		}
+
 		if (fileStream.isStream())
 			callback('Stream not supported yet');
 	});
